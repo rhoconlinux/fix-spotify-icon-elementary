9d1753178388237063cf1e01d75ba2d67da1058e
rhoconlinux <rhoconlinux@gmail.com>
2016-03-05 18:54:46 -0300

    / ‘install.sh’

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
+cd /tmp/
+
 set -e
 
 wget -O fix-spotify-icon.sh https://raw.githubusercontent.com/faviouz/fix-spotify-icon/master/src/fix-spotify-icon.sh
 
 chmod +x fix-spotify-icon.sh
 
-sudo mv fix-spotify-icon.sh /usr/local/bin/fix-spotify-icon
+sh fix-spotify-icon.sh
+
+cd
+clear
